0f1f72c6165a0fe95a5d6b0d137359cbbd722c16
Homulilly <143877220+RailgunGLM@users.noreply.github.com>
2024-11-26 20:29:35 +0000

    Add files via upload

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Update pip
+echo "Updating pip..."
+python3.12 pip install -U pip
+
+# Install dependencies
+
+echo "Installing project dependencies..."
+python3.12 -m pip install -r requirements.txt
+
+# Make migrations
+echo "Making migrations..."
+python3.12 manage.py makemigrations --noinput
+python3.12 manage.py migrate --noinput
+#Install whitenoise
+echo Installing white noise..."
+python3.12 manage.py whitenoise
+# Collect staticfiles
+echo "Collect static..."
+python3.12 manage.py collectstatic --noinput --clear
+
+echo "Build process completed!"```
+
+wsgi config :
