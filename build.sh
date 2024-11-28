14eb103d32e3d1813effcbff1f64b23f2ec013d0
Homulilly <143877220+RailgunGLM@users.noreply.github.com>
2024-11-28 20:20:56 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ python3.12 pip install -U pip
 # Install dependencies
 
 echo "Installing project dependencies..."
-python3.12 -m pip install requirements.txt
-python3.12 -m pip install pkgconfig
+python3.12 -m pip install -r requirements.txt
+python3.12 -m pip install -r pkgconfig
 
 # Make migrations
 echo "Making migrations..."
